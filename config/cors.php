682e3641f3a57ea101ff968e64215b5620be912d
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2026-03-02 03:45:19 +0900

    Update CORS configuration for allowed origins
    Commented out specific allowed origins and added wildcard origin.

--- config/cors.php
+++ config/cors.php
@@ -5,12 +5,18 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://omamori-frontend-react.vercel.app',
-        'https://omamori-frontend-react-pudabk9dq-dayeon2423004s-projects.vercel.app',
-        'http://localhost:3000',
-        'http://localhost:5173',
-    ],
+    // 'allowed_origins' => [
+    //     'https://omamori-frontend-react.vercel.app',
+    //     'https://omamori-frontend-react-pudabk9dq-dayeon2423004s-projects.vercel.app',
+    //     'http://localhost:3000',
+    //     'http://localhost:5173',
+    // ],
+    
+    // 'allowed_origins_patterns' => [
+    // '#^https://omamori-frontend-react-.*\.vercel\.app$#', // 이 패턴이 모든 vercel 프리뷰 주소를 잡아줍니다.
+    // ],
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
